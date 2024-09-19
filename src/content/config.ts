0cd6acef85a7d8e10c0454f4721497c5d4df84ea
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-09-19 06:21:29 +0200

    feat(projects): add dedicated pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,6 +19,8 @@ const projectSchema = z.object({
   slug: z.string(),
   desc: z.string(),
   img: z.string(),
+  url: z.string().optional(),
+  badge: z.string().optional(),
 });
 
 const experimentSchema = z.object({
