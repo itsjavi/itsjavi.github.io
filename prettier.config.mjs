77e542bfab16bcff9bf34857f0b699c5110e116d
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-09-19 09:27:08 +0200

    chore: configure prettier properly

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  printWidth: 120,
+  proseWrap: "always",
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
